64c07c75edd48b26f95a0722257b0dcb322fd6a0
Gonzalo <villalbagonzalo154@gmail.com>
2024-10-14 21:00:37 -0300

    Se sube el proyecto vacio

---
+++ src/main/java/com/itec/FitPower/FitPowerApplication.java
@@ -0,0 +1,13 @@
+package com.itec.FitPower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitPowerApplication.class, args);
+	}
+
+}
